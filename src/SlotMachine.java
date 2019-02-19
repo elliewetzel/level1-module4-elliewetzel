b3fa7a8bd98f56417acb2e6f8b9aa349e38775d9
Anjruski <league@WorthyMini-5.attlocal.net>
2019-02-18 16:54:54 -0800

    SlotMachine

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,28 +1,34 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.net.URL;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class SlotMachine {
+public class SlotMachine implements MouseListener {
 	Icon Strawberry;
 	Icon Grapes;
 	Icon Watermelon;
+	JLabel slot1 = new JLabel();
+	JLabel slot2 = new JLabel();
+	JLabel slot3 = new JLabel();
+	int reel1, reel2, reel3;
+	JFrame frame = new JFrame();
 	public static void main(String[] args) {
 	SlotMachine slot = new SlotMachine();
 	slot.frameBuilder();
-	
 }
 	
 	private void frameBuilder() {
-		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		JButton button = new JButton("SPIN");
-		frame.add(panel);
-		panel.add(button);
+		button.addMouseListener(this);
 		
 		URL imageURL = getClass().getResource("Strawberry.png");
 		Strawberry = new ImageIcon(imageURL);
@@ -30,18 +36,70 @@ public class SlotMachine {
 		Grapes = new ImageIcon(imageURL);
 		imageURL = getClass().getResource("Watermelon.png");
 		Watermelon = new ImageIcon(imageURL);
-		JLabel slot1 = new JLabel();
 		slot1.setIcon(Strawberry);
-		JLabel slot2 = new JLabel();
 		slot2.setIcon(Grapes);
-		JLabel slot3 = new JLabel();
 		slot3.setIcon(Watermelon);
-	
+		
+		panel.add(button);
 		panel.add(slot1);
 		panel.add(slot2);
 		panel.add(slot3);
+		frame.add(panel);
 		frame.pack();
 		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	
+	 private int showImage(JLabel label) {
+		 int r = new Random().nextInt(2);
+		 System.out.println(r);
+		 if(r == 0) {
+		label.setIcon(Strawberry);
+		 }
+		 else if(r ==  2) {
+		 label.setIcon(Grapes);
+		 }
+		 else {
+		label.setIcon(Watermelon);
+		 }
+		 return r;
+	 }
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		reel1 = showImage(slot1);
+		reel2 = showImage(slot2);
+		reel3 = showImage(slot3);
+		frame.pack();
+		if(reel1 == reel2 && reel2 == reel3){
+			JOptionPane.showMessageDialog(null, "YOU WIN!!");
+		}
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
